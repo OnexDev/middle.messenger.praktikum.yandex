ab45d0f20181822041ea817210a76082b4845260
Artyom Marchenko <marchenko.ar@icloud.com>
2022-08-27 03:59:04 +0300

    Parcel postbuild remove

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,5 +1,5 @@
 import template from './button.hbs';
 import Handlebars from 'handlebars/dist/handlebars.runtime';
-import styles from './button.scss'
+import * as styles from './button.scss'
 
 Handlebars.registerPartial('button', (props) => template({...props, styles}))
