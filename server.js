38314dc52f6b588c26f4988d3d9600e57a445117
Artyom Marchenko <marchenko.ar@icloud.com>
2022-08-28 21:03:03 +0300

    Routing fix

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const fallback = require('express-history-api-fallback');
 const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use('*', express.static(path.join(__dirname, './dist')))
+app.use(express.static(path.join(__dirname, './dist')))
+app.use(fallback('index.html', { root: `${__dirname}/dist` }))
 
 app.listen(PORT, function () {
   console.log(`Messenger app listening on port ${PORT}!`)
