4d18cfbb63dd3ec9f3391223d41b7d0118aeeeab
Artyom Marchenko <marchenko.ar@icloud.com>
2022-09-09 13:18:49 +0300

    Add field

---
+++ src/components/field/index.ts
@@ -0,0 +1,29 @@
+import template from './button.hbs';
+import * as styles from './button.scss';
+import Block from '../../utils/Block';
+import { BlockProps } from '../../utils/models/BlockProps';
+import getPropsWithAugmentedClasses from '../../utils/atomic/getPropsWithAugmentedClasses';
+
+interface FieldProps extends BlockProps{
+    label: string,
+    isPrimary?: boolean,
+    events?: {
+        click: () => void;
+    }
+}
+
+export default class Field extends Block {
+  constructor(props: FieldProps) {
+    super('div', getPropsWithAugmentedClasses<FieldProps>(props, [styles.button], [
+      props.isPrimary ? styles.button : false,
+    ]));
+  }
+
+  init() {
+
+  }
+
+  render() {
+    return this.compile(template, { ...this.props, styles });
+  }
+}
